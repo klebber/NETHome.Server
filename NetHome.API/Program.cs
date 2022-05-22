651fe975afe6cda472777fbb2c747a477681fbed
klebber <bstojev199@gmail.com>
2022-05-22 15:04:22 +0200

    Remove Startup.cs

--- NetHome.API/Program.cs
+++ NetHome.API/Program.cs
@@ -1,26 +1,102 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using NetHome.Common.JsonConverters;
+using NetHome.Common.Models;
+using NetHome.Core.Services;
+using NetHome.Data.Entities;
+using NetHome.Data;
+using System.Security.Claims;
+using NetHome.Core.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using NetHome.API.Middleware;
 
-namespace NetHome.API
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddAuthentication(options =>
 {
-    public class Program
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
     {
-        public static void Main(string[] args)
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        RequireExpirationTime = true
+    };
+    options.Events = new JwtBearerEvents
+    {
+        OnTokenValidated = async context =>
         {
-            CreateHostBuilder(args).Build().Run();
+            var id = context.Principal.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var userManager = context.HttpContext.RequestServices.GetRequiredService<UserManager<User>>();
+            var user = await userManager.FindByIdAsync(id) ?? throw new AuthorizationException("Unable to verify token!");
+            var claims = new ClaimsIdentity();
+            foreach (string role in await userManager.GetRolesAsync(user))
+                claims.AddClaim(new Claim(ClaimTypes.Role, role));
+            context.Principal.AddIdentity(claims);
         }
+    };
+});
+services.AddAuthorization(options =>
+{
+    options.AddPolicy("ElevatedRights", policy => policy.RequireRole("Owner", "Admin"));
+});
+services.AddDbContext<NetHomeContext>(options => options.UseSqlite(builder.Configuration["ConnectionStrings:NetHomeContextConnection"]));
+services.AddIdentityCore<User>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequiredLength = 4;
+}).AddRoles<IdentityRole>()
+  .AddEntityFrameworkStores<NetHomeContext>();
+services.AddScoped<IHttpRequestHandler, HttpRequestHandler>();
+services.AddSingleton<IWebSocketManager, WebSocketManager>();
+services.AddScoped<IWebSocketHandler, WebSocketHandler>();
+services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+services.AddScoped<IUserService, UserService>();
+services.AddScoped<ITokenService, TokenService>();
+services.AddScoped<IDeviceService, DeviceService>();
+services.AddScoped<IStateChangeNotifyService, StateChangeNotifyService>();
+services.AddScoped<IDeviceStateService, DeviceStateService>();
+services.AddControllers()
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    options.JsonSerializerOptions.Converters.Add(new RuntimeTypeConverter<DeviceModel>("NetHome.Common.Models.Devices.", "NetHome.Common"));
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+var app = builder.Build();
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseWebSockets(new WebSocketOptions()
+{
+    KeepAliveInterval = TimeSpan.FromSeconds(10)
+});
+app.UseMiddleware<WebSocketMiddleware>();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
